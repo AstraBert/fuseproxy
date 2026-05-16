dd181c5982cbcfd0e54c8fd21e52d03630b005d2
Clelia (Astra) Bertelli <clelia@runllama.ai>
2026-05-16 16:09:53 +0200

    chore: pre-publish

---
+++ .changeset/slow-cats-read.md
@@ -0,0 +1,5 @@
+---
+"@cle-does-things/fuseproxy": minor
+---
+
+first release
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,43 @@
-import { runServer } from "./api";
+#!/usr/bin/env node
 
-runServer().catch(console.error);
+import { anthropic, runServer } from "./api";
+import { program } from "commander";
+import { VERSION } from "./version";
+
+program
+  .name("fuseproxy")
+  .description(
+    "CLI to run a Langfuse-instrumented proxy for Anthropic's Message API (and compatible providers)",
+  )
+  .version(VERSION);
+
+program
+  .command("run")
+  .description(
+    "Run the server, optionally specifiying the Anthropic API key and base URL.",
+  )
+  .option(
+    "-k, --key <string>",
+    "API key for Anthropic (or compatible provider)",
+    undefined,
+  )
+  .option(
+    "-u, --url <string>",
+    "Base URL for Anthropic (or compatible provider)",
+    undefined,
+  )
+  .action(async (options) => {
+    if (options.key) {
+      anthropic.apiKey = options.key;
+    }
+    if (options.url) {
+      anthropic.baseURL = options.url;
+    }
+    try {
+      await runServer();
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+program.parseAsync();
